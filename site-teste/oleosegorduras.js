bc7eaf0c109d3fe098d65df214b3f05e585ad7ca
Amanda Gonsalves <64379712+amandagonsalves@users.noreply.github.com>
2020-05-01 14:14:55 -0300

    alterações no js

--- site-teste/oleosegorduras.js
+++ site-teste/oleosegorduras.js
@@ -10,18 +10,16 @@ function registrar(){
     if(usuario.value.length==0||senha.value.length==0||senha.value.length<8) {
         alert('Insira seus dados para o registro. A senha deve ter no mínimo 8 dígitos.')
     } else {
-        res.innerHTML=`O usuário ${usuario.value} foi registrado. Sua senha é ${senha.value}`
+        res.innerHTML=`O usuário ${usuario.value} foi registrado. Sua senha é ${senha.value}.`
     } 
     usuario.value=''
     usuario.focus()
     senha.value=''
     senha.focus()
 }
-
-
 //------------------------------------------------------------------------------------------
 
-//usa-se isso para mudar a foto no mouseenter e mouseout e é completa no html
+//para mudar a foto no mouseenter e mouseout
 function mudaFoto(foto) {
     document.querySelector('img#imagemoleo').src=foto
 }
